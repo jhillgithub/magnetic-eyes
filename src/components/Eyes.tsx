486506ce741e5a706e6318cfe9b2af2d1c89d740
jh <>
2024-08-12 19:17:36 -0700

    refactor to use hook

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -1,36 +1,10 @@
-import {
-  MotionValue,
-  motion,
-  useMotionValue,
-  useSpring,
-  useTransform,
-} from "framer-motion";
+import { motion, useTransform } from "framer-motion";
 import React from "react";
-import { remapRange } from "../lib/utils";
-
-const MAGNETIC_STRENGTH = 32.0;
+import { useMagneticCoords } from "../hooks/useMagneticCoords";
 
 export const Eyes = () => {
-  const setTransform = (
-    item: HTMLElement & EventTarget,
-    event: React.PointerEvent,
-    x: MotionValue,
-    y: MotionValue
-  ) => {
-    const { left, top, width, height } = item.getBoundingClientRect();
-    const relativeX = event.clientX - left;
-    const relativeY = event.clientY - top;
-    const xRange = remapRange(relativeX, 0, width, -1, 1);
-    const yRange = remapRange(relativeY, 0, height, -1, 1);
-    xPoint.set(xRange * MAGNETIC_STRENGTH);
-    yPoint.set(yRange * MAGNETIC_STRENGTH);
-  };
-  const spring = { damping: 2, stiffness: 100, restDelta: 0.001 };
-  const xPoint = useMotionValue(0);
-  const yPoint = useMotionValue(0);
-  const x = useSpring(xPoint, spring);
-  const y = useSpring(yPoint, spring);
-
+  const { x, y, setTransform } = useMagneticCoords();
+  // adjust eye movement strength
   const eyeX = useTransform(x, (latest) => latest * 0.33);
   const eyeY = useTransform(y, (latest) => latest * 0.38);
 
---
+++ src/hooks/useMagneticCoords.ts
@@ -0,0 +1,33 @@
+import { MotionValue, useMotionValue, useSpring } from "framer-motion";
+import React from "react";
+import { remapRange } from "../lib/utils";
+
+const MAGNETIC_STRENGTH = 32.0;
+
+export const useMagneticCoords = () => {
+  const setTransform = (
+    item: HTMLElement & EventTarget,
+    event: React.PointerEvent,
+    x: MotionValue,
+    y: MotionValue
+  ) => {
+    const { left, top, width, height } = item.getBoundingClientRect();
+    const relativeX = event.clientX - left;
+    const relativeY = event.clientY - top;
+    const xRange = remapRange(relativeX, 0, width, -1, 1);
+    const yRange = remapRange(relativeY, 0, height, -1, 1);
+    xPoint.set(xRange * MAGNETIC_STRENGTH);
+    yPoint.set(yRange * MAGNETIC_STRENGTH);
+  };
+  const spring = { damping: 2, stiffness: 100, restDelta: 0.001 };
+  const xPoint = useMotionValue(0);
+  const yPoint = useMotionValue(0);
+  const x = useSpring(xPoint, spring);
+  const y = useSpring(yPoint, spring);
+
+  return {
+    x,
+    y,
+    setTransform,
+  };
+};
