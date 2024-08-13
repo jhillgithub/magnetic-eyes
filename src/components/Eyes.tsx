f824b3a2494fa1982cf4853ffe3c3a2a58ff5d81
jh <>
2024-08-12 19:10:08 -0700

    refinements

--- src/components/Eyes.tsx
+++ src/components/Eyes.tsx
@@ -1,5 +1,4 @@
 import {
-  AnimatePresence,
   MotionValue,
   motion,
   useMotionValue,
@@ -32,39 +31,37 @@ export const Eyes = () => {
   const x = useSpring(xPoint, spring);
   const y = useSpring(yPoint, spring);
 
-  const textX = useTransform(x, (latest) => latest * 0.33);
-  const textY = useTransform(y, (latest) => latest * 0.38);
+  const eyeX = useTransform(x, (latest) => latest * 0.33);
+  const eyeY = useTransform(y, (latest) => latest * 0.38);
 
   return (
     <>
-      <AnimatePresence>
-        <motion.div
-          className="h-screen aspect-square bg-green-200 grid place-content-center"
-          onPointerMove={(event) => {
-            const item = event.currentTarget;
-            setTransform(item, event, x, y);
-          }}
-          onPointerLeave={(event) => {
-            x.set(0);
-            y.set(0);
-          }}
-        >
-          <motion.div style={{ x, y }} className="flex gap-4">
-            <div className="w-40 h-40 rounded-full bg-white border-black  border-2 p-2 grid place-content-center">
-              <motion.div
-                className="ml-4 rounded-full bg-black w-28 h-28"
-                style={{ x: textX, y: textY }}
-              ></motion.div>
-            </div>
-            <div className="w-40 h-40 rounded-full bg-white border-black  border-2 p-2 grid place-content-center">
-              <motion.div
-                className="mt-4 -ml-4 rounded-full bg-black w-28 h-28"
-                style={{ x: textX, y: textY }}
-              ></motion.div>
-            </div>
-          </motion.div>
+      <motion.div
+        className="h-screen aspect-square bg-green-200 grid place-content-center"
+        onPointerMove={(event) => {
+          const item = event.currentTarget;
+          setTransform(item, event, x, y);
+        }}
+        onPointerLeave={(event) => {
+          x.set(0);
+          y.set(0);
+        }}
+      >
+        <motion.div style={{ x, y }} className="flex gap-4">
+          <div className="w-40 h-40 rounded-full bg-white border-black  border-2 p-2 grid place-content-center">
+            <motion.div
+              className="ml-4 rounded-full bg-black w-28 h-28"
+              style={{ x: eyeX, y: eyeY }}
+            ></motion.div>
+          </div>
+          <div className="w-40 h-40 rounded-full bg-white border-black  border-2 p-2 grid place-content-center">
+            <motion.div
+              className="mt-4 -ml-4 rounded-full bg-black w-28 h-28"
+              style={{ x: eyeX, y: eyeY }}
+            ></motion.div>
+          </div>
         </motion.div>
-      </AnimatePresence>
+      </motion.div>
     </>
   );
 };
